b24de59c7290fbbcb3826dcba7771cc5fb3b4ca0
Vikash7080 <sengarvikash256@gmail.com>
2025-07-13 19:40:08 +0530

    Navbar and footer

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,14 +1,34 @@
+import React from "react"
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 function App() {
  
 
   return (
     <>
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element= {<Body/> }/>
+     
+      <Route path="/login" element= {<Login/> }/>
+     
+      <Route path="/profile" element= {<Profile/> }/>
+     
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
+     
+ 
       
-      <h1>Hello World</h1>
+    
      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
