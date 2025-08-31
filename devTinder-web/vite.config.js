75bcfde445ebf86cfd29bb626b8fb6187c11c03b
Vikash7080 <sengarvikash256@gmail.com>
2025-08-31 16:06:47 +0530

    fixed constants.js

--- devTinder-web/vite.config.js
+++ devTinder-web/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
-});
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  }
+})
